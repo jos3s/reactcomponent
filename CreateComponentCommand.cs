52c1bc5b3e780e385e62464f232e048965a40d19
José Ulisses <julissesmacedo@outlook.com>
2024-01-30 23:51:35 -0300

    reactcomponent parte 1

---
+++ CreateComponentCommand.cs
@@ -0,0 +1,17 @@
+﻿using Spectre.Console.Cli;
+
+namespace reactcomponent;
+internal class CreateComponentCommand : Command<CreateComponentSettings>
+{
+    public override int Execute(CommandContext context, CreateComponentSettings settings)
+    {
+        Console.WriteLine($"nome do component {settings.ComponentName}");
+        return 0;
+    }
+}
+
+internal class CreateComponentSettings : CommandSettings
+{
+    [CommandArgument(0, "<componentName>")]
+    public string ComponentName { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using reactcomponent;
+
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+var app = new CommandApp();
+
+app.Configure(app =>
+{
+    app.AddCommand<CreateComponentCommand>("create");
+});
+
+app.Run(args);
+
